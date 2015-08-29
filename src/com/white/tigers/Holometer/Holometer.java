781f5bad614a2cfe90eee61c615a1954a3574c4d
mremi <mark.remi@sparcedge.com>
2015-08-29 04:26:03 -0400

    setting pictures

--- src/com/white/tigers/Holometer/Holometer.java
+++ src/com/white/tigers/Holometer/Holometer.java
@@ -1,54 +1,49 @@
 package com.white.tigers.Holometer;
 
 import android.app.Activity;
-import android.content.Context;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Holometer extends Activity {
     /**
      * Called when the activity is first created.
      */
-
-    WeatherService weatherService;
-    LocationManager locationManager;
-    LocationListener locationListener;
-    Weather currentWeather;
-
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        weatherService = new WeatherService(getApplicationContext());
+        // Set Weather
+        setWeather(R.drawable.rain_white);
 
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                updateLocation(location);
-            }
+        setSpeedLimit(R.drawable.fast65);
 
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
     }
 
-    private void updateLocation(Location location)
-    {
-        currentWeather = weatherService.getWeather(location);
+
+    /**
+     * Set speed limit.
+     * @param speedLimit
+     */
+    private void setSpeedLimit(int speedLimit) {
+        ImageView topSpeed = (ImageView) findViewById(R.id.topSpeedLimit);
+        ImageView bottomSpeed = (ImageView) findViewById(R.id.bottomSpeedLimit);
+
+        topSpeed.setImageResource(speedLimit);
+        bottomSpeed.setImageResource(speedLimit);
+    }
+
+
+    /**
+     * Set Weather
+     *
+     * @param weather
+     */
+    private void setWeather(int weather) {
+        ImageView topWeather = (ImageView) findViewById(R.id.topWeather);
+        ImageView bottomWeather = (ImageView) findViewById(R.id.bottomWeather);
+
+        topWeather.setImageResource(weather);
+        bottomWeather.setImageResource(weather);
     }
 }
